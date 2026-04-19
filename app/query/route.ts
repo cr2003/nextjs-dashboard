80f8b1fce4e36f20773404c86d50c70c9089100b
cr2003 <crodrigo2003@gmail.com>
2026-04-19 13:44:19 +0200

    'query latest'

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,22 +1,27 @@
-// import { sql } from '@vercel/postgres';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
-  const data = await sql`
+	const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
 
-  return data.rows;
+	return data;
 }
 
-// CRITICAL: Ensure "export" is here!
+
 export async function GET() {
+  // return Response.json({
+  //   // message:
+  //     // 'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
   try {
-    const invoices = await listInvoices();
-    return Response.json(invoices);
+  	return Response.json(await listInvoices());
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+  	return Response.json({ error }, { status: 500 });
   }
 }
