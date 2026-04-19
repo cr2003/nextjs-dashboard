f3799522480f3faf15ccdd530ca3acc50f9b2700
cr2003 <crodrigo2003@gmail.com>
2026-04-19 14:04:01 +0200

    "query 19/04/2026 14:03"

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,27 +1,16 @@
-import postgres from 'postgres';
+import { sql } from '@vercel/postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// DELETE the 'import postgres' line
+// DELETE the 'const sql = postgres...' line
 
 async function listInvoices() {
-	const data = await sql`
+  // Now this uses the 'sql' we imported on Line 1
+  const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
 
-	return data;
-}
-
-
-export async function GET() {
-  // return Response.json({
-  //   // message:
-  //     // 'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  // });
-  try {
-  	return Response.json(await listInvoices());
-  } catch (error) {
-  	return Response.json({ error }, { status: 500 });
-  }
+  return data.rows;
 }
